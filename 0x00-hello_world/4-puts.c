f57f33b550cf2b7a4b835dbd8b88650abd4b4ef1
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-02 07:37:05 -0700

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
-   char str1[] = "Programming is like building a multilingual puzzle";
-   puts(str1);
+int main(void) {
+
+char str1[] = "Programming is like building a multilingual puzzle";
+
+puts(str1);
 
  return 0;
 } 
