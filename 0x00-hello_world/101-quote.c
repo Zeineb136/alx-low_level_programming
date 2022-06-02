0b065cb8c12c150efb9cc774baa36391fc342441
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-02 15:18:30 -0700

    C programming

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+/**
+** main-Entry point
+*
+** Return: Always 0 (Success)
+*/
+
+
+
+int main(void)
+
+{
+
+char char = "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n";
+putchar(char);
+
+return (1);
+
+}
