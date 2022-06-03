b5a09e5ec0bc0c3f869e386edbd9fd67e6257e8f
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-03 14:17:31 -0700

    C programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+**main-Entry point
+*
+**Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+
+char x;
+for (x = 'a'; x <= 'z'; x++)
+{
+if (x != 'e' && x != 'q')
+putchar (x);
+}
+
+putchar ('\n');
+return (0);
+
+}
