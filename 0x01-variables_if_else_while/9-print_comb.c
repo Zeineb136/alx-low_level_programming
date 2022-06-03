86d0eec2077015e134bfc3a202c5b9e9ad8d978f
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-03 16:18:30 -0700

    C programming

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+**main- print all single digit number of base 10 starting from 0
+*
+**Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int num;
+
+for (num = 0 ; num < 10 ; num++)
+{
+putchar (num + '0');
+if (num != 9)
+{putchar (' ');
+putchar (',');
+}
+}
+putchar ('\n');
+return (0);
+
+}
