c67e049b7bdecd985e170fb89c96bcae38f6da92
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-10 16:21:18 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -10,28 +10,26 @@
 
 void print_triangle(int size)
 {
-	int i, j;
+	int i, j, m;
 
 	if (size > 0)
 	{
 		for (i = 0 ; i < size ; i++)
 		{
-			for (j = 0 ; j < size ; j++)
+			for (j = size - 1; j > i ; j--)
 			{
-				if (j < size - (a + 1))
-				{
-					_putchar(' ');
-				}
-				else
+				_putchar(' ');
+			}
+
+			for (m = 0; m < i + 1 ; m++)
 				{
 					_putchar (35);
 				}
-			}
 			_putchar('\n');
 		}
 	}
 	else
 	{
-		_putchar ('\n');
+	_putchar('\n');
 	}
 }
