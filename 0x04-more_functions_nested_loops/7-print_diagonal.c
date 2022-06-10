0c4ad6cbf9913bea32729afc16da88a6424e7664
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-10 12:21:11 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-*print_line -function that draws a straight line in the terminal.
+*print_diagonal -function that draws a straight line in the terminal.
 *
 *@n: the number of character \ should be printed
 *
 *Return: line or 0
 */
 
-void print_line(int n)
+void print_diagonal(int n)
 {
 	int start = 1;
 
