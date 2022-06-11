241d09519860a7861aea3817c07598bb017128f8
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-10 18:04:38 -0700

    print integer

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+
+/**
+*print_number - function that prints an integer.
+*
+*@n: number
+*
+*Return: integer.
+*/
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		putchar(45);
+		i = -i;
+	}
+	if (i > 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
