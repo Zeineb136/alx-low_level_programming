e3e69f0717b0e4f950fd990de2284b79d863917a
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-16 16:00:45 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+*leet -function that encodes a string into 1337.
+*@str: string
+*
+*Return: string of number
+*
+*/
+
+char *leet(char *str)
+{
+	int i, j;
+
+	char c[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+	
+	j = 0;
+	while (c[j] != '\0')
+	{
+		if (str[i] == c[j])
+		{
+			str[i] = num[i];
+		}	
+		j++;
+	}
+	i++;
+
+	}
+	return (str);
+}
