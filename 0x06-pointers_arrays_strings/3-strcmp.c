05fc8a204bed730d64df16faaa3d1b3f8aa05d8b
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-16 08:49:18 -0700

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*_strcmp -function that compares two strings.
+*
+*@s1: string1
+*@s2: string2
+*
+*Return: 0 if equal or D if different
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int i, D;
+
+	i = D = 0;
+
+	while (s1[i] != '\0' && a == 0)
+	{
+		D = s1[i] - s2[i];
+		i++;
+	}
+	return (D);
+}
