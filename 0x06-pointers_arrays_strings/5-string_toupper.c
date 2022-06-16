e3c22235707b823718b41bf6466ba1ddefef5539
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-16 14:21:50 -0700

    uppercase string

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*string_toupper -function that changes all lowercase letters
+*of a string to uppercase.
+*@str: input string
+*
+*Return: uppercase string
+*/
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && stri[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
