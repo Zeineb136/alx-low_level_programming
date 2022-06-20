98b52be5e04360b20ab898423fece8698e795fac
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-20 14:32:46 -0700

    strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -17,14 +17,15 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
+			if (needle[j] == '\0')
+			{
+				return (haystack);
+			}
 			if (needle[j] != haystack[i + j])
 				break;
 
 			if (needle[i + j] == '\0')
 				return (haystack + i);
-
-			if (needle[j] == '\0')
-				return (haystack);
 		}
 	}
 
