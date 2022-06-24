b9c2c81b5c481952e6a43d0c5ef0aeadcc6838cf
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-24 14:11:14 -0700

    sum

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+*main -adds positive numbers.
+*
+*@argc: number of arguments passed to the function
+*@argv:argument vector
+*
+*Return: always 0.
+*/
+
+int main(int argc, char *argv[])
+{
+	int i, sum;
+
+	if (argc < 1)
+	{
+		return (0);
+	}
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		if (i != atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
+
