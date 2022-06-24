c9a08f0629b6dbd79dfaf6d13cf0d8692b837cba
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-24 14:58:54 -0700

    sum

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,17 +14,11 @@ int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
 
-	if (argc < 1)
-	{
-		return (0);
-	}
-
-
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!atoi(argv[i]))
+			if (argv[i][j] < 47 || argv[i][j] > 57)
 			{
 				printf("Error\n");
 				return (1);
