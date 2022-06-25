de28ee8330b889b12cfaa656de31383b411815e8
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-24 17:27:08 -0700

    static librairies

--- 0x09-static_libraries/2-strlen.c
+++ 0x09-static_libraries/2-strlen.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-*_strlen_recursion -function that returns the length of a string.
+*_strlen -function that returns the length of a string.
 *
 *@s:string
 *
 *Return: length of a string.
 */
 
-int _strlen_recursion(char *s)
+int _strlen(char *s)
 {
 	if (*s == '\0')
 	{
@@ -16,7 +16,7 @@ int _strlen_recursion(char *s)
 	}
 	else
 	{
-		return (1 + _strlen_recursion(s + 1));
+		return (1 + _strlen(s + 1));
 	}
 }
 
