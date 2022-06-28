d4a3aa33f8305eb17599dac27bd96512803b95d5
Zeineb136 <moussa.zeineb1@gmail.com>
2022-06-28 14:02:29 -0700

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 
 	array = malloc(sizeof(char) * size);
 
-	if (size == 0 || *array == '\0')
+	if (size == 0 || array == NULL)
 		return ('\0');
 
 	for (i = 0; i < size; i++)
