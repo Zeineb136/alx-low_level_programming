77edc3bc48617e87034a0aca7c4a410e076f19f8
Zeineb136 <moussa.zeineb1@gmail.com>
2022-07-04 01:20:55 -0700

    name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - print the name of the file it was compiled from
+*
+*Return: name of the file
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
