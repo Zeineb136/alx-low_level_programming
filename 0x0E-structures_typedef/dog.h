9900f6c4867647333830250694d0c3495cfb57d5
Zeineb136 <moussa.zeineb1@gmail.com>
2022-07-05 06:40:03 -0700

    typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,7 +15,11 @@ struct dog
 	float age;
 	char *owner;
 };
-
+/**
+*dog_t - typedef for struct
+*/
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
 #endif
