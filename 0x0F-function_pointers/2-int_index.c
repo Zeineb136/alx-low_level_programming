2f7568c6828d342e495b8a2c7d3a6ca70a533323
Zeineb136 <moussa.zeineb1@gmail.com>
2022-07-06 19:50:09 -0700

    array iterator

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+*int_index - function that searches for an integer.
+*@array: array
+*@size: the number of elements in the array
+*@cmp: a pointer to the function.
+*
+*Return: nothing.
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp)
+	{
+		for (i = 0; i < (int)size; i++)
+			if (cmp[i] != 0)
+				return (i);
+		return (-1);
+	}
+}
