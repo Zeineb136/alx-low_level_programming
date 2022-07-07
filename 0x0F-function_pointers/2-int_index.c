943dfab6c63f8db6f49383aa35a1fb712fe4dbb4
Zeineb136 <moussa.zeineb1@gmail.com>
2022-07-06 19:57:35 -0700

    array iterator

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,8 +16,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array && cmp)
 	{
 		for (i = 0; i < size; i++)
-			if (cmp(array[i]) != 0)
+		{
+			if (cmp(array[i]))
 				return (i);
+		}
 		return (-1);
 	}
 }
