c53644a93f0563b4f1bc7141578826aaf7513ca8
Zeineb136 <moussa.zeineb1@gmail.com>
2022-07-15 10:48:35 -0700

    print before main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 void first_print (void) __attribute__ ((constructor));
-
+/**
+*first_print - function that prints string before the main function is executed
+*
+*Return: void.
+*/
 void first_print (void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
