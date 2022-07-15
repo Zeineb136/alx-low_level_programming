1280c495cd40a86bead451acfdc89f3debf18d72
Zeineb136 <moussa.zeineb1@gmail.com>
2022-07-15 08:57:15 -0700

    free list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -21,6 +21,8 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	first_node = malloc(sizeof(list_t));
+	if (first_node == NULL)
+		return (NULL);
 	first_node->str = strdup(str);
 
 	for (i = 0; str[i] != '\0'; i++)
