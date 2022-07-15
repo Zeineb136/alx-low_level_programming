2701096c6780b803730deb7a1b0e3daeecfe7847
Zeineb136 <moussa.zeineb1@gmail.com>
2022-07-15 08:38:01 -0700

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h> 
+/**
+*free_list - function that frees a list_t list.
+*@head: pointer to the head
+*
+*Return: void.
+*/
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;	
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h> 
 /**
 *struct node -singly linked list
 *@str: string
@@ -22,4 +23,5 @@ size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 #endif
