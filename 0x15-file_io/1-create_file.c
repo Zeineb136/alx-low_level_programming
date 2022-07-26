2d52e9307035b896ef72def2049ae82a6b5574a4
Zeineb136 <moussa.zeineb1@gmail.com>
2022-07-26 11:07:46 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 
 	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
