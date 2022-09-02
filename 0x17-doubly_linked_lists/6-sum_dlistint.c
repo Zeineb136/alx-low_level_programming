1f382250b157444098ef6eb0bda287e09f6ab71a
Zeineb136 <moussa.zeineb1@gmail.com>
2022-09-01 19:44:11 -0700

    3DLL3

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *sum_dlistint- function that returns the sum of all the data (n) of a DLL
+ *@head: pointer to the head
+ *
+ * Return: sum of all data or return 0 if list is empty
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
